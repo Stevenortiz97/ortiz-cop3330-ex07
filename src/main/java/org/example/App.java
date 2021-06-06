639af555f0a77e44ff82362d3685c2e710822d97
Steven Ortiz <stevenortiz@knights.ucf.edu>
2021-06-05 22:15:07 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,61 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 7
+ *  Copyright 2021 Steven Ortiz
+ */
+
+//What is the length of the room in feet? 15
+//What is the width of the room in feet? 20
+//You entered dimensions of 15 feet by 20 feet.
+//The area is
+//300 square feet
+//27.871 square meters
+
+
+import java.util.Scanner;
+
+import java.text.DecimalFormat;
+
+public class App {
+
+    static Scanner in = new Scanner(System.in);
+
+    private static int lengthint(){
+
+        System.out.print("What is the length of the room in feet? ");
+        String length = in.nextLine();
+        return Integer.parseInt(length);
+
+    }
+
+    private static int widthint(){
+
+        System.out.print("What is the width of the room in feet? ");
+        String width = in.nextLine();
+        return Integer.parseInt(width);
+
+    }
+
+    public static void main(String[] args) {
+
+        double factor = (double) 0.09290304;
+
+        int lengthint = lengthint();
+        int widthint = widthint();
+
+        System.out.println("You entered the dimensions of " + lengthint + " feet by " + widthint + " feet.");
+
+        int areaImperial = lengthint * widthint;
+        double areaMetric = areaImperial * factor;
+
+        System.out.println("The area is");
+        System.out.println(+areaImperial+" square feet");
+
+        DecimalFormat format = new DecimalFormat("0.000");
+        System.out.print(format.format(areaMetric));
+        System.out.print(" square meters");
+
+    }
+
+}
